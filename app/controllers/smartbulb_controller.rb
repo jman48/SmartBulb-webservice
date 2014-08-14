32e5ce4b25c60a4555eb6228d0bcae0eaca5d555
jman48 <j.armstrong484@gmail.com>
2014-08-14 01:48:38 +0000

    Added more RESTful actions and some more routes

--- app/controllers/smartbulb_controller.rb
+++ app/controllers/smartbulb_controller.rb
@@ -1,8 +1,17 @@
 class SmartbulbController < ApplicationController
     require 'limitless_led'
+    before_action :setUp
     
     def on
-        bridge = LimitlessLed::Bridge.new(host: '121.73.64.157', port: 8899)
-        bridge.all_on        # all lights are on
+        @bridge.all_on        # all lights are on
     end
+    
+    def off
+        @bridge.all_off        # all lights are on
+    end
+    
+    private
+        def setUp
+            @bridge = LimitlessLed::Bridge.new(host: '121.73.64.157', port: 8899)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     get 'smartbulb/on' => 'smartbulb#on'
+    get 'smartbulb/off' => 'smartbulb#off'
     
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
